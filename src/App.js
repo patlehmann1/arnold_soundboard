d69a426f3950756f9449ce96de82c5419fc99295
pat <patlehmann1@gmail.com>
2018-11-14 00:44:51 -0500

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Arnold from './Arnold';
 
 class App extends Component {
   render() {
@@ -8,9 +9,16 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+            {Arnold.responses.map(sound => {
+              return (
+              <div key={sound.name}>
+              <audio controls>
+                <source src='./sounds/responses/cop.wav' type="audio/wav"></source>
+              </audio>
+              <p>{sound.caption}</p>
+              </div>
+              )
+            })}
           <a
             className="App-link"
             href="https://reactjs.org"
---
+++ src/Arnold.js
@@ -0,0 +1,163 @@
+const Arnold = {
+    "responses": [
+        {
+            "name": "im_a_cop",
+            "wavFile": "./sounds/responses/cop.wav",
+            "caption": "I'm a cop you idiot!"
+        },
+        {
+            "name": "deep_trouble",
+            "wavFile": "./sounds/responses/deep_trouble.wav",
+            "caption": "One of us is in deep trouble."
+        },
+        {
+            "name": "fist",
+            "wavFile": "./sounds/responses/fist.wav",
+            "caption": "...enough room for my fist!"
+        },
+        {
+            "name": "no_deal",
+            "wavFile": "./sounds/cop.wav",
+            "caption": "No deal"
+        },
+        {
+            "name": "no_sh",
+            "wavFile": "./sounds/responses/no_sh.wav",
+            "caption": "No shit"
+        },
+        {
+            "name": "police",
+            "wavFile": "./sounds/responses/police.wav",
+            "caption": "Hey, I'm a police officer"
+        },
+        {
+            "name": "sob",
+            "wavFile": "./sounds/responses/sob.wav",
+            "caption": "You son of a bitch!"
+        },
+        {
+            "name": "stop_it",
+            "wavFile": "./sounds/responses/stop_it.wav",
+            "caption": "STOP IT!"
+        },
+        {
+            "name": "stop_whining",
+            "wavFile": "./sounds/responses/stop_whining_x.wav",
+            "caption": "Stop Whining!"
+        },
+        {
+            "name": "clothes",
+            "wavFile": "./sounds/responses/t1_clothes_x.wav",
+            "caption": "Your clothes, give them to me"
+        },
+        {
+            "name": "lack_discipline",
+            "wavFile": "./sounds/responses/lack_discipline.wav",
+            "caption": "You lack discipline!"
+        },
+        {
+            "name": "trust_me",
+            "wavFile": "./sounds/responses/t2_trust_me.wav",
+            "caption": "Trust me"
+        },
+        {
+            "name": "dont_do_dat",
+            "wavFile": "./sounds/responses/t1_clothes_x.wav",
+            "caption": "Don't do dat"
+        },
+        {
+            "name": "correct",
+            "wavFile": "./sounds/responses/t2_correct.wav",
+            "caption": "Correct"
+        },
+        {
+            "name": "fu_asshole",
+            "wavFile": "./sounds/responses/t1_fu.wav",
+            "caption": "Fuck you, asshole"
+        },
+        {
+            "name": "you_are_mine",
+            "wavFile": "./sounds/responses/you_are_mine.wav",
+            "caption": "...you are mine now"
+        },
+        {
+            "name": "yeah",
+            "wavFile": "./sounds/responses/yeah.wav",
+            "caption": "Yeah"
+        },
+        {
+            "name": "ugly",
+            "wavFile": "./sounds/responses/ugly.wav",
+            "caption": "One ugly MFer"
+        },
+        {
+            "name": "excellent",
+            "wavFile": "./sounds/responses/t1_excellent.wav",
+            "caption": "Excellent"
+        },
+        {
+            "name": "address",
+            "wavFile": "./sounds/responses/t1_give_me_your_address.wav",
+            "caption": "Give me your address"
+        },
+        {
+            "name": "learning_computer",
+            "wavFile": "./sounds/responses/t2_learning_computer_x.wav",
+            "caption": "Learning computer"
+        },
+        {
+            "name": "be_back",
+            "wavFile": "./sounds/responses/t1_be_back.wav",
+            "caption": "I'll be back"
+        },
+        {
+            "name": "whats_the_matter",
+            "wavFile": "./sounds/responses/whats_the_matter.wav",
+            "caption": "What's the matter?"
+        },
+        {
+            "name": "why_not",
+            "wavFile": "./sounds/responses/why_not.wav",
+            "caption": "Why not?"
+        }
+    ],
+    "openers_questions": [
+        {
+            "name": "daddy",
+            "wavFile": "./sounds/openers_questions/daddy.wav",
+            "caption": "Who is your daddy?"
+        },
+        {
+            "name": "get_to_know",
+            "wavFile": "./sounds/openers_questions/get_to_know.wav",
+            "caption": "Just get to know you"
+        },
+        {
+            "name": "good_morning",
+            "wavFile": "./sounds/openers_questions/good_morning.wav",
+            "caption": "Good Morning"
+        },
+        {
+            "name": "how_are_you",
+            "wavFile": "./sounds/openers_questions/how_are_you.wav",
+            "caption": "How are you?"
+        },
+        {
+            "name": "mother_get",
+            "wavFile": "./sounds/openers_questions/mother_get.wav",
+            "caption": "Get your mother, please"
+        },
+        {
+            "name": "mother_talk",
+            "wavFile": "./sounds/openers_questions/mother_talk.wav",
+            "caption": "Let me talk to your mother"
+        },
+        {
+            "name": "questions",
+            "wavFile": "./sounds/openers_questions/questions.wav",
+            "caption": "Ask you a bunch of questions"
+        }
+    ]
+};
+
+export default Arnold;
